50433b4d816ca15fd58e6262f5ada5ca30670935
anjali <anjalikrshna609@gmail.com>
2021-05-24 16:46:52 +0530

    bank server created

---
+++ index.js
@@ -0,0 +1,26 @@
+const exp=require('express');
+const app=exp();
+
+//GET-READ
+app.get('/',(req,res)=>{
+    res.send("THIS IS A GET METHOD");
+});
+//POST-CREATE
+app.post('/',(req,res)=>{
+    res.send("THIS IS A POST METHOD");
+});
+//PUT-UPDATE/MODIFY WHOLE
+app.put('/',(req,res)=>{
+    res.send("THIS IS A PUT METHOD");
+});
+//PATCH-UPDATE/MODIFY PARTIALLY
+app.patch('/',(req,res)=>{
+    res.send("THIS IS A PATCH METHOD");
+});
+//DELETE-DELETE
+app.delete('/',(req,res)=>{
+    res.send("THIS IS A DELETE METHOD");
+});
+app.listen(3000,()=>{
+    console.log("Server started at port 3000");
+})
